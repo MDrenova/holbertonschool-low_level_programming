ae8e0bc8f5ec422e7232fac57bcfa79d1c58343a
MDrenova <mateodrenova@gmail.com>
2023-10-16 08:36:36 -0700

    Update new Print numbers with char

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-        int num = 0;
-	while(num <= 9)
+	int num = 0;
+
+	while (num <= 9)
 	{
-        	putchar('0' + num);
-        	num++;
-	}    
-    	putchar('\n');
-        return (0);
+		putchar('0' + num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
