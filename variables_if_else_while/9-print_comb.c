f3df01a3abdc9fe6ff418d3eecd85cd84098710a
MDrenova <mateodrenova@gmail.com>
2023-10-16 05:20:25 -0700

    Print numbers with comma

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Print numbers base 16
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 0;
+	while (num <= 9)
+	{
+		putchar('0' + num);
+		if (num == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+
+	}
+	putchar('\n');
+        return (0);
+}
