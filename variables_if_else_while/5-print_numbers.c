8236078fad1f2b8ff88ca29319612a7cafc2cc82
MDrenova <mateodrenova@gmail.com>
2023-10-16 03:54:52 -0700

    Print naumber base 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Print number from 0 to 9
+ * Anothe ex:
+ * int num = 0;
+ *       while (num <10){
+ *          printf("%d", num);
+ *           num++;
+ *       }
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char num = '0';
+	while(num <= '9')
+	{
+        	putchar(num);
+        	num++;
+	}    
+    	putchar('\n');
+        return (0);
+}
