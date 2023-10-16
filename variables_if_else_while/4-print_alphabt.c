fc2bfe4e91f7fb8af6c0eed0d89f4fbc932226a8
MDrenova <mateodrenova@gmail.com>
2023-10-16 03:46:31 -0700

    Print alphabet except e and q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Print alphabet lower and upper case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char ch = 'a';
+	while(ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			ch++;
+			continue;	
+		}
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
