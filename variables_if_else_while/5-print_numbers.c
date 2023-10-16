8a3162a3130a3ed6f4943838d807574b4623f97d
MDrenova <mateodrenova@gmail.com>
2023-10-16 08:33:58 -0700

    Update Print number base 10

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -12,12 +12,13 @@
  */
 int main(void)
 {
-        char num = '0';
-	while(num <= '9')
+	char num = '0';
+
+	while (num <= '9')
 	{
-        	putchar(num);
-        	num++;
-	}    
-    	putchar('\n');
-        return (0);
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
 }
