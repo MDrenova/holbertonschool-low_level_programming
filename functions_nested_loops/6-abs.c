82d13140bfe7f78525d87914178b158fd1f0a46e
MDrenova <mateodrenova@gmail.com>
2023-10-18 04:14:07 -0700

    Absolute number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - Entry point
+ * @n: is parameter
+ * return abs(n);
+ * Return:0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * (-1));
+}
