8fdb56c4bafbd4704c436b90b8a21c88c483f2e6
MDrenova <mateodrenova@gmail.com>
2023-10-18 09:20:17 -0700

    Sum of to int numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @first_n: -first number
+ * @second_n: second num
+ * Return the sum of two int numbers;
+ * Return: 0
+ */
+int add(int first_n, int second_n)
+{
+	return (first_n + second_n);
+}
