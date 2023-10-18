6ca4ed1a5d69b262145110108bf774e47d0b5f05
MDrenova <mateodrenova@gmail.com>
2023-10-18 06:33:32 -0700

    Multi table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ * @n: is parameter
+ * return abs(n);
+ * Return:0
+ */
+
+int void times_table(void)
+{
+	int i, j, multi;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			multi = i * j;
+			if (multi >= 10)
+			{
+				_putchar (',');
+				_putchar (' ');
+				_putchar (multi / 10 + '0');
+				_putchar (multi % 10 + '0');
+			}
+			else
+			{
+				if (j == 0)
+				{
+					_putchar (multi + '0');
+				}
+				else
+				{
+					_putchar (',');
+					_putchar (' ');
+					_putchar (' ');
+					_putchar (multi + '0');
+				}
+			}
+		}
+	}
+}
