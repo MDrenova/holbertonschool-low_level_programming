e49a82f22e57fe9d58f74b50c0b6d24a1de1bf79
MDrenova <mateodrenova@gmail.com>
2023-10-19 04:02:31 -0700

    Create file: initiate function to make multiplication and main

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * main.h header file
+ * @a: is parameter first num to make multiplication
+ * @b: parameter second num to make multiplication
+ * Return:0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
