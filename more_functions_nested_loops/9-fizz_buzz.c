4e8f1364b25570e31f11ad0f538918c1925a5357
MDrenova <mateodrenova@gmail.com>
2023-10-19 11:35:21 -0700

    Create file Fizz-Buzz (multiplies of 3, 5)

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main- function entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s%s", fizz, buzz);
+		else if (i % 3 == 0)
+			printf("%s ", fizz);
+		else if (i % 5 == 0)
+			printf("%s ", buzz);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+
+	return (0);
+}
