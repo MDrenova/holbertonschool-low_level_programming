f9887f571c878dc74c87d8a794a9967890f26d10
MDrenova <mateodrenova@gmail.com>
2023-10-19 04:17:26 -0700

    Create file  that prints the numbers, from 0 to 9 and main to exe function

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - function that multiplies two integers
+ * main.h header file
+ * Return:0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
