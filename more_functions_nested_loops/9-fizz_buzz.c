2ac8fde1ce5eb02228b3f90ca9835f1abff8c6bc
MDrenova <mateodrenova@gmail.com>
2023-10-20 01:16:57 -0700

    Update fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i == 100)
+		{
+			printf("%s", buzz);
+			break;
+		}
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("%s%s ", fizz, buzz);
 		else if (i % 3 == 0)
 			printf("%s ", fizz);
 		else if (i % 5 == 0)
 			printf("%s ", buzz);
-		else if (i == 100)
-			printf("%d", i);
 		else
 			printf("%d ", i);
 	}
