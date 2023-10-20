7fb016e6743be84d9ec1a1bf90945a6d30b8f364
MDrenova <mateodrenova@gmail.com>
2023-10-20 02:24:47 -0700

    Create function that prints a triangle, followed by a new line

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - print 90deg triangle
+ * @size: input from user until when to print #
+ * i,j,k for iterations cycles
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 0; k <= i; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
