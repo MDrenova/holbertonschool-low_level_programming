1edb1e6d2ccd34eef8768a19225b92e7341e80e1
MDrenova <mateodrenova@gmail.com>
2023-10-23 11:04:51 -0700

    Create file to copy array

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * *_strcpy - copy an array
+ * @dest: entered value for destination array
+ * @src: array that will be copied
+ * Return: the copied array
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (src[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
