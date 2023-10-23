de8c5943a4b3987197f4551a308533015404f224
MDrenova <mateodrenova@gmail.com>
2023-10-23 04:17:52 -0700

    Print string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - print a string
+ * @str: entered value
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
