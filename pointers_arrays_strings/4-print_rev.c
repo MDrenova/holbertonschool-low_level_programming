4ef400248d46903c7b257e865f21c64a0735565e
MDrenova <mateodrenova@gmail.com>
2023-10-23 04:44:09 -0700

    Create file reverse string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print a string
+ * @s: entered value
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+	int i, last_index;
+
+	while (s[len] != 0)
+		len++;
+	last_index = len - 1;
+
+	for (i = last_index; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
