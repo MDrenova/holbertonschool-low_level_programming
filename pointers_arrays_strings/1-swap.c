19558fd265de66bf7102674fd817da89673b7c35
MDrenova <mateodrenova@gmail.com>
2023-10-23 03:21:38 -0700

    Create a file that make a swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - function swap value
+ * @a: entered value
+ * @b: enter another value
+ * Return - void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
