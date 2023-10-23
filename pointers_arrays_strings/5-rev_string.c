81a192963c8f30e89ff7c40a4c6c7f5ab4bea745
MDrenova <mateodrenova@gmail.com>
2023-10-23 08:55:15 -0700

    Reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rev_string - print a string
+ * @s: entered value
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int end, start;
+
+	while (s[len] != 0)
+		len++;
+	end = len - 1;
+	start = 0;
+
+	while (start < end)
+	{
+		char tmp = s[start];
+
+		s[start] = s[end];
+
+		s[end] = tmp;
+
+		start++;
+		end--;
+	}
+
+}
