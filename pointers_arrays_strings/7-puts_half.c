af490827420706327e10e7cf2e0ae49797ffbfd0
MDrenova <mateodrenova@gmail.com>
2023-10-23 09:41:10 -0700

    Update file to print half of even string and odd

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,7 +9,7 @@ void puts_half(char *str)
 	int len = 0;
 	int i;
 
-	while (str[len] != 0)
+	while (str[len] != '\0')
 		len++;
 
 	if ((len - 1) % 2 == 1)
@@ -19,5 +19,9 @@ void puts_half(char *str)
 			_putchar(str[i]);
 		}
 	}
+	else
+		for (i = len / 2 + 1; i < len; i++)
+			_putchar(str[i]);
+
 	_putchar('\n');
 }
