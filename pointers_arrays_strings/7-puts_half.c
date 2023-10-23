96e87fc49108050b479ca10ffc35afdfd0565eda
MDrenova <mateodrenova@gmail.com>
2023-10-23 09:22:26 -0700

    Create file to print half of even string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - print half of even  string
+ * @str: entered value
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != 0)
+		len++;
+
+	if ((len - 1) % 2 == 1)
+	{
+		for (i = len / 2; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
