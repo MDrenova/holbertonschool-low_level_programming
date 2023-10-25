23e80cf3c395061f903ca0af1494b6d695a62c53
MDrenova <mateodrenova@gmail.com>
2023-10-25 09:18:12 -0700

    Update encode function with one if two loop

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,29 +6,17 @@
 
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+
+	char lett[10] = {'a', 'A', 'e', 'E', 'o', 'E', 't', 'T', 'l', 'L'};
+	char num[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; j < 10; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == lett[j])
+				str[i] = num[j];
 		}
 	}
 
