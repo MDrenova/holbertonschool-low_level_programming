e795c87696814c2f12ab7a30fcbbbab8d6f1f5be
MDrenova <mateodrenova@gmail.com>
2023-10-25 05:16:26 -0700

    Create a function to make all words capitalized

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+/**
+ * cap_string - changes all lowercase letter of a string to uppercase
+ * @str: string
+ * Return: charachter
+ */
+
+char *cap_string(char *str)
+{
+	int i, j;
+	char a[] = {' ','.',',','\t','\n', '(', ')', '\\', '{', '}', '!'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] = str[i] - 32;
+		}
+		for (j = 0; j != '\0'; j++)
+		{
+
+			if (str[i] == a[j])
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+					str[i] = str[i] - 32;
+			}
+		}
+	}
+
+	return (str);
+}
