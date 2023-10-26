cdd027b526a6e8bf8fbb4a6e78b560041c65b2d6
MDrenova <mateodrenova@gmail.com>
2023-10-26 11:36:34 -0700

    Update function to create chess tabl

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-void print_chessboard(char (*a)[8]){
-    int i, j;
-    for (i = 0; i < 8; i++){
-        for (j = 0; j < 8; j++){
-            printf("%c",a[i][j]);
-        }
-        printf("\n");
-    }
-    
+/**
+ * print_chessboard - pritn a chessboard
+ * @a: pointer to a matrix
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c",a[i][j]);
+		}
+		printf("\n");
+	}
 }
