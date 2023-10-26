8e22a6759824a84aa1a605113115e9d48be9b51c
MDrenova <mateodrenova@gmail.com>
2023-10-26 04:17:23 -0700

    Create a serch to find char to str

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+/**
+ * _strchr - functions to search a char in str
+ * @s: pointer to string
+ * @c: character to fill
+ * Return: string
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + 1);
+	}
+
+	return (NULL);
+}
