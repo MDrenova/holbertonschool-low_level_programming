c5a1c6da595e6280cfcd9a0c652841ebad955302
MDrenova <mateodrenova@gmail.com>
2023-10-27 02:41:57 -0700

    Update funtion to find a substring to a string

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -3,6 +3,7 @@
  * _strstr - functions to compare a string in another string
  * @haystack: pointer to string
  * @needle: pointer a string to compare
+ * haystac_i index of the haystack need_i index  of needle
  * Return: point to nth char
  */
 char *_strstr(char *haystack, char *needle)
@@ -19,7 +20,7 @@ char *_strstr(char *haystack, char *needle)
 			need_i++;
 		}
 		if (*need_i == '\0')
-			return haystack;
+			return (haystack);
 	}
 
 	return (NULL);
