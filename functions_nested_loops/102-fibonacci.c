663af6538c8ac789a6635518e7f6d513f508591a
MDrenova <mateodrenova@gmail.com>
2023-10-29 15:01:46 -0700

    Create a function to print Fiboncci numbers

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - open fuction to make multip of 3 and 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	signed long int i, nr, nr_s, nr_fib;
+
+	nr = 0;
+	nr_s = 1;
+	nr_fib = nr + nr_s;
+	printf("%ld, ", nr_fib);
+	for (i = 0; i < 50; i++)
+	{
+		nr = nr_s;
+		nr_s = nr_fib;
+		nr_fib = nr + nr_s;
+		printf("%ld", nr_fib);
+		if(i != 49)
+			printf(", ");
+	}
+	printf("\n");
+
+	return (0);
+}
