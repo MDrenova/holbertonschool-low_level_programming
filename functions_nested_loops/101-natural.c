33d3c5e3704e648704ae663d1a589296c46cc9bd
MDrenova <mateodrenova@gmail.com>
2023-10-29 13:25:59 -0700

    Update 'Sum of multiplied number of 3 and 5

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -6,20 +6,16 @@
  */
 int main(void)
 {
-	int i, sum, sum1;
+	int i, sum;
 
 	sum = 0;
-	sum1 = 0;
 	for (i = 1; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 			sum = sum + i;
-		if (i % 3 == 0 && i % 5 == 0)
-			sum1 = sum1 + i;
 	}
 
-	sum = sum - sum1;
-	printf("Sum of multipliesa number with 3 and 5 is %d \n", sum);
+	printf("%d \n", sum);
 
 	return (0);
 }
