bf3811eb608f8f65be12b184f09634d573f42972
MDrenova <mateodrenova@gmail.com>
2023-10-30 05:16:08 -0700

    Create a function to calculate the factorial of a given nr

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function to return length of string
+ *
+ * @s: string pointer
+ *
+ * Return: 1 for 0 value, faktorial number if n > 0 and -1 for negative input; 
+ */
+int factorial(int n)
+{
+	if (n == '0')
+	{
+		return (1);
+	}
+	else if (n > 0)
+		return (n * factorial(n - 1));
+	else
+		return (-1);
+}
