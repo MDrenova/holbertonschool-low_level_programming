771122e1410f0e1ce121d8ac3db4ebd9ab9ea616
MDrenova <mateodrenova@gmail.com>
2023-11-01 05:33:39 -0700

    Update comment

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -3,6 +3,7 @@
  * main - main funcion
  * @argc: arguments past in unix count
  * @argv: strings pasted
+ * ne vend te int argc mund te __attribute__((unused)) argc
  * Return: Return 0
  */
 int main(int argc, char *argv[])
