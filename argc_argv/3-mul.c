1040274f6699fc380abeff25f5c63a4ee15fc287
MDrenova <mateodrenova@gmail.com>
2023-11-01 05:23:16 -0700

    Update betty

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -9,18 +9,20 @@
 int main(int argc, char *argv[])
 {
 	int i, mul;
+
 	mul = 1;
-	
-	if (argc > 1){
+	if (argc > 1)
+	{
 		for (i = 1; i < argc; i++)
 		{
 			mul = mul * atoi(argv[i]);
 		}
 		printf("%d\n", mul);
-		return 0;
+		return (0);
 	}
-	else{
-		printf("Error\n");	
+	else
+	{
+		printf("Error\n");
 		return (1);
 	}
 }
