52ac963b92d962902f819661f4bcba03630367e3
MDrenova <mateodrenova@gmail.com>
2023-11-01 04:29:55 -0700

    Create a file that print its namefile argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main funcion
+ * @argc: arguments past in unix count
+ * @argv: strings pasted
+ * Return: Return 0
+ */
+int main(int argc, char *argv[])
+{
+	int arg = argc - 1;
+
+	printf("%s\n", argv[arg]);
+	return (0);
+}
