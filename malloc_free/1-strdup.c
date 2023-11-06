21c159f5fb48c0628bd8d9eb1b58f724b3c58118
MDrenova <mateodrenova@gmail.com>
2023-11-06 03:55:36 -0800

    Update: condition

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -15,10 +15,10 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < len; i++)
-		s[i] = str[i];
 	if (s == NULL)
 		return (NULL);
+	for (i = 0; i < len; i++)
+		s[i] = str[i];
 	s[i] = '\0';
 	return (s);
 }
