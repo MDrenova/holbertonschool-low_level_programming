02a0fb48cd21c4e6ce47cca9f8bf5633e56ba659
MDrenova <mateodrenova@gmail.com>
2023-11-06 05:28:13 -0800

    Update function to concat strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,17 +10,15 @@ char *str_concat(char *s1, char *s2)
 {
 	int i;
 	int len = strlen(s1);
-	int len2 = strlen(s2); 
+	int len2 = strlen(s2);
 	char *s;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
 	s = malloc(sizeof(char) * (len + len2 + 1));
-	for (i = 0; i <= len; i++)
+	for (i = 0; i < len; i++)
 		s[i] = s1[i];
 	for (i = 0; i < len2; i++)
 		s[len + i] = s2[i];
