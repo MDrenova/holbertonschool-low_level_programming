7f94809959b6191230fc88f51147795e7edd10e5
MDrenova <mateodrenova@gmail.com>
2023-11-06 05:05:09 -0800

    Update function to concat strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strdup - function to create an arry
- * @str: sie to duplicate
+ * str_concat - function to concate 2 arry
+ * @s1: first string
+ * @s2: second string
  * Return: null or a pointer to s
  */
 char *str_concat(char *s1, char *s2)
@@ -12,6 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int len2 = strlen(s2); 
 	char *s;
 
+	if (s1 == NULL)
+		return (s2);
+	if (s2 == NULL)
+		return (s1);
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	s = malloc(sizeof(char) * (len + len2 + 1));
