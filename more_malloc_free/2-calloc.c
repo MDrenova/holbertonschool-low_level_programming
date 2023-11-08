1ef3b39de88fd3d5e7d0011e5bebd00f8ee1ff3b
MDrenova <mateodrenova@gmail.com>
2023-11-08 04:03:45 -0800

    Update declaration

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -9,11 +9,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
+	char *arr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	char *arr = malloc(size * nmemb);
+	arr = malloc(size * nmemb);
 
 	if (arr == NULL)
 		return (NULL);
