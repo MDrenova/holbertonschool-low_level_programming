a4b7d3ab58789b6d7be79882e23146aec93ea66d
MDrenova <mateodrenova@gmail.com>
2023-11-13 05:14:04 -0800

    Update library

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,17 +9,17 @@
 
 void print_dog(struct dog *d)
 {
-	if(d != 0)
+	if (d != 0)
 	{
-		if(d->name == NULL)
+		if (d->name == NULL)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
-		if(d->age == 1E-15)
+		if (d->age == 1E-15)
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
-		if(d->owner == NULL)
+		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", d->owner);
