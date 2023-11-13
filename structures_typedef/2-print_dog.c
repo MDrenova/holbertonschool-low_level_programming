1ba7d3d9bc97fcf2dd2cb6d435861d92ada4f3d6
MDrenova <mateodrenova@gmail.com>
2023-11-13 06:21:15 -0800

    Update condition

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,8 +18,6 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		if (d->age)
 			printf("Age: %f\n", d->age);
-		else
-			printf("Age: (nil)\n");
 		if (d->owner == NULL)
 			printf("Owner: (nil)\n");
 		else
