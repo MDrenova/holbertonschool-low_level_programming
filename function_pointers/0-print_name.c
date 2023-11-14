40b95319906ffcfd72419e5962d6223748e7be4d
MDrenova <mateodrenova@gmail.com>
2023-11-14 05:08:18 -0800

    Create function to print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print-name - functio to print the name
+ * @name: name para string
+ * @f: function pointer
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
