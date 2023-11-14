2dcac473b2f0643fd46549a821943b6538ba55a3
MDrenova <mateodrenova@gmail.com>
2023-11-14 05:15:31 -0800

    Update betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,15 @@
 #include <stddef.h>
 
 /**
- * print_name - functio to print the name
- * @name: name para string
- * @f: function pointer
+ * print_name - function that prints a name
+ * @name: name
+ * @f: f
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
+	{
 		f(name);
+	}
 }
