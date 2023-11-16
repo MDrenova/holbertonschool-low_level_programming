ff8256e93ad5ace85997474d237580f4977295c6
MDrenova <mateodrenova@gmail.com>
2023-11-16 03:44:35 -0800

    Create fun to calc sum and header file

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum of arg past to funct
+ * @n: number of int tht will be past
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, sum;
+	va_list num;
+
+	va_start(num, n);
+	sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(num, int);
+	}
+
+	return (sum);
+
+}
