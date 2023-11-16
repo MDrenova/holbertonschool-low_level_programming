e3172dc66712d6165b9c3fbbbcc27f9527028ded
MDrenova <mateodrenova@gmail.com>
2023-11-16 04:37:16 -0800

    Create func to print arg

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+
+/**
+ * print_numbers - print arguments
+ * @separator: pointer to string
+ * @n: number of arg
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list print_num;
+
+	va_start(print_num, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(print_num, int));
+		if ((*separator) != NULL)
+			if (i < n - 1)
+				printf("%s", separator);
+	}
+	printf("\n");
+	va_end(num);
+
+}
