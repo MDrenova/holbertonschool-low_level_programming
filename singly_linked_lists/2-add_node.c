f161430f74f051b9e7a1a36737414f7274004a4c
MDrenova <mateodrenova@gmail.com>
2023-11-28 13:51:28 -0800

    Create funtion to add a node to the biginig of the list

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Double pointer to the head of the list.
+ * @str: String to be included in the new node.
+ *
+ * Return: Address of the new element, or NULL if it failed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_mode;
+	size_t len = strlen(str);
+
+	new_node = malloc(sizeof(list_n));
+	if (!new_mode)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (!new_node)
+	{
+		free(new_mode);
+		return (NULL);
+	}
+
+	new_mode->len = len;
+	new_node->next = *head;
+	*head = new_mode;
+
+	return (new_node);
+}
