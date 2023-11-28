ae4f03f5aa13bef854f463c509385214a059b6f7
MDrenova <mateodrenova@gmail.com>
2023-11-28 13:56:37 -0800

    Correct typescripting

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	size_t len = strlen(str);
 
-	new_node = malloc(sizeof(list_n));
+	new_node = malloc(sizeof(list_t));
 	if (!new_mode)
 		return (NULL);
 
