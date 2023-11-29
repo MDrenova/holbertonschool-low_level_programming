00ac6ecfc5a5bb9f4de5aa1ca100516afb72eceb
MDrenova <mateodrenova@gmail.com>
2023-11-29 11:59:12 -0800

    Add inicialization and return valu to condition and change the order of linking the lasts node

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,6 +1,5 @@
 #include "lists.h"
 /**
- *
  * add_dnodeint_end - Adds a new node at the end of a dlistin_t list.
  * @head: Double pointer to the head of the list.
  * @n: integer number to be included in the new node.
@@ -17,17 +16,20 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
 	new_node->next = NULL;
 
 	if (!*head)
+	{
+		new_node->prev = NULL;
 		*head = new_node;
+		return (new_node);
+	}
 
 	while (last_node->next)
 		last_node = last_node->next;
 
-	last_node->next = new_node;
 	new_node->prev = last_node;
+	last_node->next = new_node;
 
 	return (new_node);
 }
