fded1d13c0f6cf3b9c6f496ab45daaca94598fef
MDrenova <mateodrenova@gmail.com>
2023-12-01 02:24:38 -0800

    Update conditions and declare the new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,8 +16,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
 	if (idx == 0)
-		return (add_dnodeint(h, n));
+	{
+		new_node->next = *h;
+		if (*h !- NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
+		return (new_node);
+	}
 
 	while (current_node != NULL && i < idx)
 	{
