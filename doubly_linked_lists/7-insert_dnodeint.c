a432dc085c638ad41f73e998c419b20eec7edf04
MDrenova <mateodrenova@gmail.com>
2023-12-01 02:00:31 -0800

    Create function to create a function to add at nth index a new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,29 +12,30 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current_node = *h;
 	unsigned int i;
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 0; i < idx; i++)
+	{
+		if (!current_node)
+			return (NULL);
+		current_node = current_node->next;
+	}
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = current_node;
+	new_node->prev = current_node->prev;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-	i = 0;
-	for (i = 0; i <= idx - 1; i++)
-	{
-		if (!current_node)
-		{
-			add_dnodeint_end(h, n);
-			return (new_node);
-		}
-		current_node = current_node->next;
-	}
+	if (current_node->prev != NULL)
+		current_node->prev->next = new_node;
+	else
+		*h = new_node;
 
-	new_node->prev = current_node;
-	new_node->next = current_node->next;
-	current_node = new_node;
-	current_node->next->prev = new_node;
+	current_node->prev = new_node;
 
 	return (new_node);
 }
