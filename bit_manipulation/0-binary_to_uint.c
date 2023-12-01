e827f5d66f7cbf525cdeb5473c36fb4a845f20c0
MDrenova <mateodrenova@gmail.com>
2023-12-01 05:58:55 -0800

    Update conditon for null string

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,12 +12,12 @@ unsigned int binary_to_uint(const char *b)
 	int binary_base = 1, sum = 0;
 	int len = strlen(b), i;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		if ((b[i] != '0' && b[i] != '1') || !b)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
 			sum = sum + binary_base;
