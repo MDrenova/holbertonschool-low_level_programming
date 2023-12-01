f748a44aaec5d5022bf7449df829713d60cbc442
MDrenova <mateodrenova@gmail.com>
2023-12-01 02:17:32 -0800

    Create function to create a function to add at nth index a new node

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -8,34 +8,32 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node;
-	dlistint_t *current_node = *h;
-	unsigned int i;
+	dlistint_t *new_node, *current_node = *h;
+	unsigned int i = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(dlistint_t));
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (i = 0; i < idx; i++)
+	while (current_node != NULL && i < idx)
 	{
-		if (!current_node)
-			return (NULL);
+		if (i == idx - 1)
+		{
+			new_node->next = current_node->next;
+			new_node->prev = current_node;
+			if (current_node->next != NULL)
+				current_node->next->prev = new_node;
+			current_node->next = new_node;
+			return (new_node);
+		}
+		i++;
 		current_node = current_node->next;
 	}
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->next = current_node;
-	new_node->prev = current_node->prev;
-
-	if (current_node->prev != NULL)
-		current_node->prev->next = new_node;
-	else
-		*h = new_node;
-
-	current_node->prev = new_node;
-
-	return (new_node);
+	free(new_node);
+	return (NULL);
 }
