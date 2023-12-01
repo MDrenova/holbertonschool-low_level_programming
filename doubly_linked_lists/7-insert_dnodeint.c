4fb53e66b088846dc062cf269974021bd6ba5ca1
MDrenova <mateodrenova@gmail.com>
2023-12-01 02:29:06 -0800

    Create function to insert node at nth index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - Return the sum.
- * @h: double pointer to the head of the list.
- * @idx: index where the node will be add
- * @n: number to fill new node
- * Return: Address of the new element, or NULL if it failed.
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Double pointer to the first node of the double linked list.
+ * @idx: Index of the list where the new node should be added.
+ * @n: Integer to be included in the new node.
+ *
+ * Return: Address of the new node, or NULL if it failed.
  */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *current_node = *h;
+	dlistint_t *new_node, *temp = *h;
 	unsigned int i = 0;
 
 	if (h == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
@@ -24,29 +25,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = NULL;
 
 	if (idx == 0)
-	{
-		new_node->next = *h;
-		if (*h != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
+		return (add_dnodeint(h, n));
 
-	while (current_node != NULL && i < idx)
+	while (i++ < idx)
 	{
-		if (i == idx - 1)
+		if (temp == NULL)
 		{
-			new_node->next = current_node->next;
-			new_node->prev = current_node;
-			if (current_node->next != NULL)
-				current_node->next->prev = new_node;
-			current_node->next = new_node;
+			free(new_node);
+			return (NULL);
+		}
+		if (i == idx)
+		{
+			new_node->next = temp->next;
+			new_node->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = new_node;
+			temp->next = new_node;
 			return (new_node);
 		}
-		i++;
-		current_node = current_node->next;
+		temp = temp->next;
 	}
-
 	free(new_node);
 	return (NULL);
 }
