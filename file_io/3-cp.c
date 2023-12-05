a422d76a94a0507ff5c848b5eadbdd53d4dabfd6
MDrenova <mateodrenova@gmail.com>
2023-12-05 07:14:39 -0800

    Update function close

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,6 +6,25 @@
 #define BUFFER_SIZE 1024
 
 /**
+ * close_file - close file
+ * @fd1: file form
+ * @fd2: file to
+ */
+void close_file(int fd1, int fd2)
+{
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+}
+/**
  * main - copies the content of a file to another file
  * @argc: argument count
  * @argv: argument vector
@@ -60,23 +79,4 @@ int main(int argc, char *argv[])
 	}
 	close_file(fd_from, fd_to);
 	return (0);
-}
-/**
- * close_file - close file
- * @fd1: file form
- * @fd2: file to
- */
-void close_file(int fd1, int fd2)
-{
-	if (close(fd1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
-
-	if (close(fd2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
-	}
 }
