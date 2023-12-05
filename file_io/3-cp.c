4f19c62cd9ddb0366ea6e0127045d5a3adca96e5
MDrenova <mateodrenova@gmail.com>
2023-12-05 10:07:50 -0800

    Update varaibles name

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (asrgv[1] == NULL)
+	if (argv[1] == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
