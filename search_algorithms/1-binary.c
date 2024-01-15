fb5e5471a2084cc5fa6bb3dc6ee737b648cdf57b
MDrenova <mateodrenova@gmail.com>
2024-01-15 06:09:26 -0800

    Create binary search function that searches for a value in a sorted array of integers

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+#include <stdlib.h>
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @start: index where to start the index
+ * @end: end of array
+ */
+void print_array(const int *array, int start, int end)
+{
+	int i = start;
+
+	printf("Searching in array: ");
+	while (array && i < end)
+	{
+		printf("%d", array[i]);
+		if (i < end - 1)
+			printf(", ");
+		++i;
+	}
+	printf("\n");
+}
+
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * @array: array that will be searched in
+ * @size: size of array
+ * @value: value to search in array
+ * Return: (-1) for null or not find or the index otherwise
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0, right = size - 1, medium;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (left <= right)
+	{
+		medium = (left + right) / 2;
+		print_array(array, left, right + 1);
+		if (array[medium] < value)
+			left = medium + 1;
+		else if (array[medium] > value)
+			right = m - 1;
+		else
+			return (medium);
+	}
+	return (-1);
+}
